6a321415e7004b49820edae5569cd26e13bbe1c7
DavidCarvajal1 <david.carvajal@iesnervion.es>
2022-11-17 10:38:32 +0100

    Ejercicio completado

--- src/Hilo.java
+++ src/Hilo.java
@@ -13,4 +13,13 @@ public class Hilo extends Thread {
             }
         } while (true);
     }
+
+    public static void main(String[] args) {
+        //Lanzamos 5 veces el hilo con un nombre distinto
+        for (int i = 0; i < 5; i++) {
+            Thread h=new Hilo();
+            h.setName("Hilo "+(i+1));
+            h.start();
+        }
+    }
 }
